c7c98f0e7c6376f0918c8718fc6e26b76ce0fea6
R2BT <theeraphat.a@ku.th>
2021-03-25 18:27:09 +0700

    3

--- Palindrome_6320500573.c
+++ Palindrome_6320500573.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main()
 {
-    int n,i,sum=0;
+    int n,i,j,sum=0;
     int p1,p2;
     scanf("%d",&n);
     int num[n],cnum[n],newnum[n],onenum[10]={1,2,3,4,5,6,7,8,9};
@@ -13,26 +13,33 @@ int main()
         cnum[i]=i+1;
         i++;
     }
+    for(i=0;i<9;i++)
+    {
+        newnum[i]=num[i];
+    }
     i=0;
+    j=9;
     while(i<n)
     {
+
         if(i>8)
         {
             p2=cnum[i]%10;
             p1=cnum[i]/10;
-            newnum[i]=(p2*10)+p1;
-
+            newnum[j]=(p2*10)+p1;
+            j++;
         }
+
         i++;
 
     }
     i=0;
     while(i<n)
     {
-        if(num[i]==onenum[i])
+        /*if(num[i]==onenum[i])
         {
             sum++;
-        }
+        }*/
         if(num[i]==newnum[i])
         {
             sum++;
